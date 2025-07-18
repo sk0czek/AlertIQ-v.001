847df998398336401ae20229b7cc5a6527808885
Kornel Skoczylas <kornel.skoczylas@icloud.com>
2025-07-18 12:28:51 +0200

    Add html support to mail

--- src/mailer.py
+++ src/mailer.py
@@ -7,28 +7,38 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SMTP_SERVER = os.getenv("SMTP_SERVER")
-SMTP_PORT = int(os.getenv("SMTP_PORT"))
+SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
 SMTP_LOGIN = os.getenv("SMTP_LOGIN")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 EMAIL_FROM = os.getenv("EMAIL_FROM")
+
+def convert_text_to_html(text):
+    return f"""<html>
+    <body>
+    <pre style="font-family: Consolas, monospace; font-size: 14px;">
+{text}
+    </pre>
+    </body>
+</html>"""
+
 
 def send_report_email(to_email, subject, body):
     if not all([SMTP_SERVER, SMTP_PORT, SMTP_LOGIN, SMTP_PASSWORD, EMAIL_FROM]):
         raise ValueError("Brakuje danych SMTP - sprawdz plik .env")
     
-    msg = MIMEMultipart()
+    msg = MIMEMultipart("alternative")
     msg["From"] = EMAIL_FROM
     msg["To"] = to_email
     msg["Subject"] = subject
-    
-    msg.attach(MIMEText(body, "plain"))
 
+    html_body = convert_text_to_html(body)
+    msg.attach(MIMEText(html_body, "html"))
 
     try:
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
             server.starttls()
             server.login(SMTP_LOGIN, SMTP_PASSWORD)
             server.send_message(msg)
-        print(f"Emial wyslany do: {to_email}")
-    except Exceptions as e:
-        print(f"Blad podczas wysylania emaila: {e}")
+        print(f"📧 Email wysłany do: {to_email}")
+    except Exception as e:
+        print(f"❌ Błąd podczas wysyłania emaila: {e}")
